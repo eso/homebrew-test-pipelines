46feeafe298575f61e3e6dead324b9c634bc84ee
Stefano Zampieri <szampier@eso.org>
2025-07-02 23:20:32 +0200

    Remove post_install method from fors recipes

--- Formula/esopipe-fors-recipes.rb
+++ Formula/esopipe-fors-recipes.rb
@@ -38,27 +38,6 @@ class EsopipeForsRecipes < Formula
     end
   end
 
-  def post_install
-    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
-    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
-    workflow_dir_1.glob("*.xml").each do |workflow|
-      ohai "Updating #{workflow}"
-      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
-        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
-      end
-      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
-        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
-      end
-      if workflow.read.include?("$ROOT_DATA_DIR/reflex_input")
-        inreplace workflow, "$ROOT_DATA_DIR/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
-      end
-      if workflow.read.include?("RAW_DATA_PATH_TO_REPLACE/")
-        inreplace workflow, "RAW_DATA_PATH_TO_REPLACE/", HOMEBREW_PREFIX/"share/esopipes/datademo/fors"
-      end
-      cp workflow, workflow_dir_2
-    end
-  end
-
   test do
     assert_match "fors_dark -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page fors_dark")
   end
