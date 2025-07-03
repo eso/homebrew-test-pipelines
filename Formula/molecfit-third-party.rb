a45f36d79e7949ac378d7a929307d0ca2c4278bf
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-07-03 19:35:03 +0000

    molecfit-third-party: add 1.9.3 bottle.

--- Formula/molecfit-third-party.rb
+++ Formula/molecfit-third-party.rb
@@ -10,6 +10,14 @@ class MolecfitThirdParty < Formula
     regex(/href=.*?molecfit_third_party[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-test-pipelines/releases/download/molecfit-third-party-1.9.3"
+    sha256 cellar: :any,                 arm64_sequoia: "7e32dbe8e294c2432ef3aee8cfc9264afdde6c90efcb13ca2f092c8e0609de15"
+    sha256 cellar: :any,                 arm64_sonoma:  "ae5297739743ff0011e86adc6074d4a41afcc27792da326ae71c27e881c7c5c8"
+    sha256 cellar: :any,                 ventura:       "6b83273b4a97c8dc1dbdbed3ec437865a3bc109f103e1cc433ee43a15d2cfb69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9f4fb0980f643396e4d9148aaf727e04d0f7c1cc0f08e8bb606b61a4542060d8"
+  end
+
   depends_on "gcc"
 
   def install
