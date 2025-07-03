4c506b04bf8279a386e6c4024b17cf36b6c98f75
Stefano Zampieri <szampier@eso.org>
2025-07-03 21:19:05 +0200

    Add molecfit-third-party
    Closes #4.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

---
+++ Formula/molecfit-third-party.rb
@@ -0,0 +1,25 @@
+class MolecfitThirdParty < Formula
+  desc "3rd party tools for the Molecfit library"
+  homepage "https://www.eso.org/sci/software/pipelines/skytools/molecfit"
+  url "https://ftp.eso.org/pub/dfs/pipelines/libraries/molecfit_third_party/molecfit_third_party-1.9.3.tar"
+  sha256 "2786e34accf63385932bad66c39deab8c8faf4c9095a23173146a9820f4f0183"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://ftp.eso.org/pub/dfs/pipelines/libraries/molecfit_third_party/"
+    regex(/href=.*?molecfit_third_party[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "gcc"
+
+  def install
+    ENV.deparallelize
+    system "make", "-f", "BuildThirdParty.mk",
+      "prefix=#{prefix}",
+      "install"
+  end
+
+  test do
+    assert_match "2460672", shell_output("echo \"2024 12 27\" | Gregorian2JD")
+  end
+end
