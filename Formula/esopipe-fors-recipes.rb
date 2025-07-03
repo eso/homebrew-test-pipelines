70e7df17f743d8103a487950cf352cb61fc9719a
Stefano Zampieri <szampier@eso.org>
2025-07-03 22:02:14 +0200

    add fors recipes

---
+++ Formula/esopipe-fors-recipes.rb
@@ -0,0 +1,65 @@
+class EsopipeForsRecipes < Formula
+  desc "ESO FORS instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
+  url "https://ftp.eso.org/pub/dfs/pipelines/kit_devel/fors-kit-5.8.4-4_svn298403_head.tar.gz"
+  sha256 "d1dfd5d40288e7ef8ff7e2e3e83a09382280fadbb2bfd221db633aac40775e36"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?fors-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  def name_version
+    "fors-#{version.major_minor_patch}"
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "cfitsio"
+  depends_on "cpl@7.3.2"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl"
+  depends_on "telluriccorr"
+
+  uses_from_macos "curl"
+
+  def install
+    system "tar", "xf", "#{name_version}.tar.gz"
+    cd name_version.to_s do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cfitsio=#{Formula["cfitsio"].prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-telluriccorr=#{Formula["telluriccorr"].prefix}",
+                            "--with-gsl=#{Formula["gsl"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  def post_install
+    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
+    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
+      ohai "Updating #{workflow}"
+      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
+      end
+      if workflow.read.include?("$ROOT_DATA_DIR/reflex_input")
+        inreplace workflow, "$ROOT_DATA_DIR/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
+      end
+      if workflow.read.include?("RAW_DATA_PATH_TO_REPLACE/")
+        inreplace workflow, "RAW_DATA_PATH_TO_REPLACE/", HOMEBREW_PREFIX/"share/esopipes/datademo/fors"
+      end
+      cp workflow, workflow_dir_2
+    end
+  end
+
+  test do
+    assert_match "fors_dark -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page fors_dark")
+  end
+end
