41991280c9afd4d0044d1efba0258637e31eceed
Stefano Zampieri <szampier@eso.org>
2025-07-03 21:26:30 +0200

    add esorex

---
+++ Formula/esorex.rb
@@ -0,0 +1,33 @@
+class Esorex < Formula
+  desc "Execution Tool for European Southern Observatory pipelines"
+  homepage "https://www.eso.org/sci/software/cpl/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/esorex-3.13.10.tar.gz"
+  sha256 "a989f9c6dbd6bbb6a9c7c678da8a3b7ad7a8c7e29c644b97c371579b45957dd6"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/"
+    regex(/href=.*?esorex[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "cpl@7.3.2"
+  depends_on "gsl"
+  depends_on "libffi"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+                          "--with-gsl=#{Formula["gsl"].prefix}",
+                          "--with-libffi=#{Formula["libffi"].prefix}",
+                          "--with-included-ltdl"
+    system "make", "install"
+    inreplace prefix/"etc/esorex.rc", prefix/"lib/esopipes-plugins", HOMEBREW_PREFIX/"lib/esopipes-plugins"
+  end
+
+  test do
+    assert_match "ESO Recipe Execution Tool, version #{version}", shell_output("#{bin}/esorex --version")
+  end
+end
