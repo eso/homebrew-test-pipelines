e9fe81e9be6c5173e14f5f5b3e86df328c16cf68
Stefano Zampieri <szampier@eso.org>
2025-07-03 22:29:42 +0200

    add esotk recipes
    Closes #9.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

---
+++ Formula/esopipe-esotk-recipes.rb
@@ -0,0 +1,62 @@
+class EsopipeEsotkRecipes < Formula
+  desc "ESO ESOTK instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/esotk/esotk-kit-0.9.9.tar.gz"
+  sha256 "020dcedaa71b1bc5016da257a20749c05d0f319d507c6e20abb5fe3a0e86b737"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?esotk-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  def name_version
+    "esotk-#{version.major_minor_patch}"
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "cpl@7.3.2"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl"
+
+  uses_from_macos "curl"
+
+  def install
+    ENV.prepend "LDFLAGS", "-L#{Formula["fftw"].opt_lib}"
+    ENV.prepend "LDFLAGS", "-L#{Formula["wcslib"].opt_lib}"
+    ENV.prepend "LDFLAGS", "-L#{Formula["cfitsio"].opt_lib}"
+
+    system "tar", "xf", "#{name_version}.tar.gz"
+    cd name_version.to_s do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+                            "--with-gsl=#{Formula["gsl"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  def post_install
+    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
+    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
+      ohai "Updating #{workflow}"
+      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE/reflex_input")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
+      end
+      cp workflow, workflow_dir_2
+    end
+  end
+
+  test do
+    assert_match "esotk_spectrum1d_combine -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page esotk_spectrum1d_combine")
+  end
+end
